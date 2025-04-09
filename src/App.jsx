f1fcbee5a3c970b5b692c6541106c64746b595b4
AyushVerma <ayush.verma@techdome.net.in>
2025-04-09 18:03:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <p className="text-center">
+        This is a simple React app with Tailwind CSS.
+      </p>
+      <p className="text-center ">
+        Edit <code>src/App.jsx</code> and save to test HMR updates.
+      </p>
+    </div>
+  );
+}
+
+export default App;
