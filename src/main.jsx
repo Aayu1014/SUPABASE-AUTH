a958445b0fd15b8695ab76e5e5b7b8d14bbe20e3
AyushVerma <ayush.verma@techdome.net.in>
2025-04-16 00:54:23 +0530

    Context Set up

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.jsx";
+import { AuthContextProvider } from "./Context/ContextAuth.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1 className="text-red-500 text-center pt-4 text-3xl">
-      Heeloooo Everyone!
-    </h1>
-    <RouterProvider router={router} />
+    <>
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
+    </>
   </StrictMode>
 );
